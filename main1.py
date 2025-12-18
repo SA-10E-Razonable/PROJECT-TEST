744ef3108e95d0893829636ff5d688b8a2cf1c91
Matt Sky L.Razonable <razonable.ml@obmontessori.edu.ph>
2025-12-18 21:39:26 +0800

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,34 +1,42 @@
-<py-script>
-from js import document
+from pyscript import display, document
 
-def show_club_info(event=None):
+club_info = {
+    "guitar": {
+        "description": "A friendly space to jam and learn new chords!",
+        "meeting_time": "Every Monday, 2:00 to 3:00 PM",
+        "location": "Room 221"
+    }
+}
+
+def show_club_info(e=None):
     container = document.getElementById("club-info-container")
+    container.innerHTML = ""
 
-    container.innerHTML = """
+    info = club_info["guitar"]
+
+    output = f"""
         <div class="row">
             <div class="col-md-6">
-                <div class="category-title">Description:</div>
-                <div class="club-description">
-                    A friendly space to jam and learn new chords!
+                <div class="category-title" style="color:#FF5733;">Description:</div>
+                <div class="club-description" style="color:#1C1C1C;">
+                    {info['description']}
                 </div>
             </div>
 
             <div class="col-md-6">
-                <div class="category-title">Time:</div>
-                <div class="club-time">
-                    Every Monday,<br>2:00 to 3:00 PM
+                <div class="category-title" style="color:#FF5733;">Time:</div>
+                <div class="club-time" style="color:#1C1C1C;">
+                    {info['meeting_time']}
                 </div>
 
-                <div class="category-title mt-3">Location:</div>
-                <div class="club-location">
-                    Room 221
+                <div class="category-title mt-3" style="color:#FF5733;">Location:</div>
+                <div class="club-location" style="color:#1C1C1C;">
+                    {info['location']}
                 </div>
             </div>
         </div>
     """
-</py-script>
 
-
-
+    display(output, target="club-info-container")
 
 
