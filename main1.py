bd5d6e2c25c332eb4daee0a53975510f491a9e31
Matt Sky L.Razonable <razonable.ml@obmontessori.edu.ph>
2025-12-18 21:27:28 +0800

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,3 +1,33 @@
+<py-script>
+from js import document
+
+def show_club_info(event=None):
+    container = document.getElementById("club-info-container")
+
+    container.innerHTML = """
+        <div class="row">
+            <div class="col-md-6">
+                <div class="category-title">Description:</div>
+                <div class="club-description">
+                    A friendly space to jam and learn new chords!
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="category-title">Time:</div>
+                <div class="club-time">
+                    Every Monday,<br>2:00 to 3:00 PM
+                </div>
+
+                <div class="category-title mt-3">Location:</div>
+                <div class="club-location">
+                    Room 221
+                </div>
+            </div>
+        </div>
+    """
+</py-script>
+
 
 
 
